b07b17bb40dc411c4346c5ada31d27334fead2d0
Arnav Vats <arnavar18@gmail.com>
2025-06-26 16:40:29 +0530

    Solving the Hosting Problem

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,45 +1,51 @@
-// import module is socket.io for Node.js
-// This code sets up a simple chat server using Socket.IO
+// Import required modules
 const express = require("express");
+const path = require("path");
 const app = express();
 const http = require("http").createServer(app);
-const io = require('socket.io')(8000, {
-// why cross-origin resource sharing (CORS) is needed
-    // CORS is needed to allow the client-side application running on a different port (e.g., 5500) to connect to the Socket.IO server running on port 8000.
+const { Server } = require("socket.io");
+
+// Create Socket.IO server on same HTTP server
+const io = new Server(http, {
     cors: {
-        origin: ["*"],
+        origin: "*", // You can set specific origin here in production
         methods: ["GET", "POST"]
     }
 });
-// this object will store the names of users connected to the chat
-const users = {};
 
-// this code listens for new connections to the Socket.IO server
+// Serve static files (HTML, CSS, JS) from root directory
+app.use(express.static(path.join(__dirname, "..")));
 
-// when a new user joins, it emits a 'user-joined' event to all connected clients
-// and stores the user's name in the 'users' object with the socket ID as the key
-io.on('connection', socket => {
-    socket.on('new-user-joined', name => {
-        users[socket.id] = name;
-        io.emit('user-joined', name); // <--- correct line
-    });
-
-// when a message is sent, it emits a 'receive' event to all connected clients
-// the message includes the message content and the name of the user who sent it
-    socket.on('send', message => {
-        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
-    
+// Fallback route to serve index.html
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "index.html"));
 });
 
-// when a user disconnects, it emits a 'leave' event to all connected clients
-// and removes the user from the 'users' object
-    socket.on('disconnect', message => {
-        socket.broadcast.emit('leave', users[socket.id]);
+// Store users
+const users = {};
+
+// Socket.IO logic
+io.on("connection", (socket) => {
+    socket.on("new-user-joined", (name) => {
+        users[socket.id] = name;
+        io.emit("user-joined", name);
+    });
+
+    socket.on("send", (message) => {
+        socket.broadcast.emit("receive", {
+            message: message,
+            name: users[socket.id],
+        });
+    });
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("leave", users[socket.id]);
         delete users[socket.id];
     });
 });
 
+// Start server on Render-assigned port
 const PORT = process.env.PORT || 8000;
 http.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
