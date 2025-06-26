c3817a4d2134a339bb439450140a9134ed13d0a5
Arnav Vats <arnavar18@gmail.com>
2025-06-26 16:27:42 +0530

    remove the express and other module

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,9 +1,5 @@
 // import module is socket.io for Node.js
 // This code sets up a simple chat server using Socket.IO
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-
 const io = require('socket.io')(8000, {
 // why cross-origin resource sharing (CORS) is needed
     // CORS is needed to allow the client-side application running on a different port (e.g., 5500) to connect to the Socket.IO server running on port 8000.
